8ab340832d2f1bf40b8845ad3b53286abf89f7cf
lazylook2 <15044013742@126.com>
2020-05-28 17:29:58 +0800

    rust

--- src/error.rs
+++ src/error.rs
@@ -1,9 +1,21 @@
 use std::fs::File;
+use std::io::ErrorKind;
 
 pub fn e1 () {
 // 使用result处理潜在的错误
-    let f = File::open("main.rs");
+// enum Result<T, E> {
+//     Ok(T),
+//     Err(E),
+// }
+    let f = File::open("fuck.txt");
     let f = match f {
-
+        Ok(file) => file,
+        Err(error) => match error.kind() {
+            ErrorKind::NotFound  => match File::create("fuck.txt") {
+                Ok(fc) => fc,
+                Err(e) => panic!("Problem creating the file: {:?}", e),
+            },
+            other_kind=> panic!("Problem opening the file: {:?}", other_error),
+        }
     };
 }
