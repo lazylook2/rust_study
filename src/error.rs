14316a620edae613cb2446358a2233116900f971
lazylook2 <15044013742@126.com>
2020-05-31 16:15:14 +0800

    rust

--- src/error.rs
+++ src/error.rs
@@ -18,4 +18,26 @@ pub fn e1 () {
             other_kind=> panic!("Problem opening the file: {:?}", other_error),
         }
     };
+    // 用了闭包
+    let f = File::open("fuck.txt").unwrap_or_else(|error| {
+        if error.kind() == ErrorKind::NotFound {
+            File::create("fuck.txt").unwrap_or_else(|error| {
+                panic!("Problem creating the file: {:?}", error);
+            })
+        } else {
+            panic!("Problem opening the file: {:?}", error);
+        }
+    });
+// 失败时 panic 的简写：unwrap 和 expect
+
+// unwrap 那样使用默认的 panic! 信息
+// expect 用来调用 panic! 的错误信息将会作为参数传递给 expect
+
+    // 如果 Result 值是成员 Ok，unwrap 会返回 Ok 中的值。
+    // 如果 Result 是成员 Err，unwrap 会为我们调用 panic!。
+    let f = File::open("hello.txt").unwrap();
+
+
+    // 使用 expect 提供一个好的错误信息可以表明你的意图并更易于追踪 panic 的根源。
+    let f = File::open("hello.txt").expect("无法打开。。。。 hello.txt");
 }
