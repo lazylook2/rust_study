2b3c4d3e01ad75c5ebcdd34aef4e4bc7d29b97d8
lazylook2 <15044013742@126.com>
2020-06-08 12:59:47 +0800

    rust

--- src/error.rs
+++ src/error.rs
@@ -85,4 +85,35 @@ pub fn e2 () {
         Err(e) => panic!("读取fuck.txt的内容失败: {:?}", e),
     }
 
+    loop {
+        // --snip--
+        let guess: i32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+        if guess < 1 || guess > 100 {
+            println!("The secret number will be between 1 and 100.");
+            continue;
+        }
+        match guess.cmp(&secret_number) {
+            // --snip--
+        }
+    }
+    pub struct Guess {
+        value: i32,
+    }
+
+    impl Guess {
+        pub fn new(value: i32) -> Guess {
+            if value < 1 || value > 100 {
+                panic!("Guess value must be between 1 and 100, got {}.", value);
+            }
+            Guess {
+                value
+            }
+        }
+        pub fn value(&self) -> i32 {
+            self.value
+        }
+    }
 }
