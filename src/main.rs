c7fd7c708d0dd5fe1f07a7d2103083a84a577f4a
lazylook2 <15044013742@126.com>
2020-06-29 22:01:17 +0800

    Box完善

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,8 @@
 use rust_study::error::e2;
 use rust_study::generics_trait::{generics, traits, lifetime};
 use rust_study::iterators_closures::Iterator;
+use rust_study::pointers::box1;
 
 fn main() {
-    Iterator();
+    box1();
 }
--- src/pointers.rs
+++ src/pointers.rs
@@ -1,8 +1,25 @@
-/// 当有一个在编译时未知大小的类型，而又想要在需要确切大小的上下文中使用这个类型值的时候
-/// 当有大量数据并希望在确保数据不被拷贝的情况下转移所有权的时候
-/// 当希望拥有一个值并只关心它的类型是否实现了特定 trait 而不是其具体类型的时候
+use std::borrow::Borrow;
+
+/// 当有一个在编译时未知大小的类型，而又想要在需要确切大小的上下文中使用这个类型值的时候 （方法里面有）
+/// 当有大量数据并希望在确保数据不被拷贝的情况下转移所有权的时候 （需要例子）
+/// 当希望拥有一个值并只关心它的类型是否实现了特定 trait 而不是其具体类型的时候 （？？？？？？）
 pub fn box1() {
     let b = Box::new(5);
     println!("b = {}", b);
 
+    enum List {
+        Cons(i32, Box<List>),
+        Nil,
+    }
+
+    use List::Cons;
+    use List::Nil;
+    let list = Cons(1, Box::new(Cons(2, Box::new(Cons(3, Box::new(Nil))))));
+    let i = 2;
+    match list {
+        Cons(i, b) => {
+            println!("i: {}", i)
+        }
+        _ => (),
+    }
 }
