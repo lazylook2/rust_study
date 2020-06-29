97d3c86d53c32dbe8b2da61eb2738997e3ec7965
lazylook2 <15044013742@126.com>
2020-06-29 16:00:30 +0800

    迭代器

--- src/iterators_closures.rs
+++ src/iterators_closures.rs
@@ -153,7 +153,53 @@ pub fn Iterator() {
     assert_eq!(in_my_size, vec![
         Shoe { size: 10, style: String::from("sneaker") },
         Shoe { size: 10, style: String::from("boot") },
-    ])
+    ]);
+// 实现 Iterator trait 来创建自定义迭代器
+    struct Counter {
+        count: u32,
+    }
+    impl Counter{
+        fn new () -> Counter{
+            Counter{ count: 0 }
+        }
+    }
+    impl Iterator for Counter{
+        type Item = u32;
 
+        fn next(&mut self) -> Option<Self::Item> {
+            self.count += 1;
+            if self.count < 6 {
+                Some(self.count)
+            } else {
+                None
+            }
+        }
+    }
+    let mut counter = Counter::new();
+
+    assert_eq!(counter.next(), Some(1));
+    assert_eq!(counter.next(), Some(2));
+    assert_eq!(counter.next(), Some(3));
+    assert_eq!(counter.next(), Some(4));
+    assert_eq!(counter.next(), Some(5));
+    assert_eq!(counter.next(), None);
+
+    // 这里调用into_iter 返回的还是Counter类型
+    let couter_iter = counter.into_iter().count;
+    println!("{}", couter_iter);
+
+// 使用自定义迭代器中其他 Iterator trait 方法
+    // 获取 Counter 实例产生的值，将这些值与另一个 Counter 实例在省略了第一个值之后产生的值配对，
+    // 将每一对值相乘，只保留那些可以被三整除的结果，然后将所有保留的结果相加
+    let sum: u32 = Counter::new().zip(Counter::new().skip(1))
+        .map(|(a, b)| a * b )
+        .filter(|x| x % 3 == 0)
+        .sum();
+    assert_eq!(18, sum);
+    // 1 * 2    2
+    // 2 * 3    6
+    // 3 * 4    12
+    // 4 * 5    20
+    // 5 * None    停止
 }
 
