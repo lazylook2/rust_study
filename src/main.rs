d8fa13d671d72c3defd9b91564a9a1d4686c7673
lazylook2 <15044013742@126.com>
2020-08-06 17:24:56 +0800

    模式

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,8 @@
 use rust_study::concurrency::threads1;
 use rust_study::concurrency::{threads2, threads3, move1, message_passing, message_passing2, message_passing3, mutex, mutex1};
 use rust_study::object_oriented::{oo1, oo2};
-use rust_study::patterns::p1;
+use rust_study::patterns::{p1, p2};
 
 fn main() {
-    p1();
+    p2()
 }
--- src/patterns.rs
+++ src/patterns.rs
@@ -53,7 +53,7 @@ pub fn p1(){
 }
 /// 所有的模式语法
 pub fn p2 () {
-    // 匹配字面值
+// 匹配字面值
     let x = 1;
     match x {
         1 => println!("1"),
@@ -61,14 +61,67 @@ pub fn p2 () {
         _ => println!("其他")
     }
 
-    // 匹配命名变量
+// 匹配命名变量
     let x = Some(5);
     let y = 10;
 
     match x {
         Some(50) => println!("到50了"),
+        // y 是在这个作用域内的新变量，匹配任何 Some 中的值，非Nome
         Some(y) => println!("匹配到了，y = {:?}", y),
         _ => println!("其他情况，x = {:?}", x)
     }
+    // 一旦 match 表达式执行完毕，其作用域也就结束了，同理内部 y 的作用域也结束了。
     println!("最后: x = {:?}, y = {:?}", x, y);
+
+// 多个模式
+    let x = 1;
+
+    match x {
+        1 | 2 | 3 => println!("1 或 2 或 3"),
+        4 => println!("4"),
+        _ => println!("其他")
+    }
+
+// 通过 ..= 匹配值的范围（..= 语法允许你匹配一个闭区间范围内的值）
+    let x = 5;
+
+    match x {
+        1 ..= 5 => println!("1 到 5"),
+        _ => println!("其他")
+    }
+
+    let x = 'c';
+    match x {
+        'a' ..= 'j' => println!("前侧的字符"),
+        'k' ..= 'z' => println!("后侧的字符"),
+        _ => println!("其他")
+    }
+
+// 解构并分解值
+// 解构结构体
+    struct Point {
+        x: i32,
+        y: i32,
+    }
+
+    let p = Point{ x: 0, y: 7 };
+    let Point{x: a, y: b} = p;
+    // 简写
+    let Point{ x, y} = p;
+    assert_eq!(0, a);
+    assert_eq!(7, b);
+    assert_eq!(x, 0);
+
+    match p {
+        // 创建了x变量
+        Point{ x, y: 0 } => println!("在x轴上的任何点 {}", x),
+        // 创建了y变量
+        Point{ x: 0, y } => println!("在y轴上的任何点 {}", y),
+        Point{ x, y} => println!("不在轴上的点 ({}, {})", x, y)
+    }
+
+// 解构枚举
+
+
 }
