59466d8df49c02e05bb4b8d528ddf7f457b2be55
lazylook2 <15044013742@126.com>
2020-09-11 18:01:04 +0800

    父 trait 用于在另一个 trait 中使用某 trait 的功能

--- src/advanced_features.rs
+++ src/advanced_features.rs
@@ -256,6 +256,41 @@ pub fn advanced_traits3(){
     // 方法无self参数
     println!("{}", <Dog as Animal>::baby_name("小狗"));
 }
+use std::fmt;
+/// 父 trait 用于在另一个 trait 中使用某 trait 的功能
+/// ???????????????????????????????????????
+pub fn advanced_traits4 (){
+    /// 因为指定了 OutlinePrint 需要 Display trait，则可以在 outline_print 中使用 to_string，其会为任何实现 Display 的类型自动实现。
+    /// 如果不在 trait 名后增加 : Display 并尝试在 outline_print 中使用 to_string，
+    /// 则会得到一个错误说在当前作用域中没有找到用于 &Self 类型的方法 to_string。
+    trait OutlinePrint: fmt::Display {
+        fn outline_print(&self) {
+            let output = self.to_string();
+            let len = output.len();
+            println!("{}", "*".repeat(len + 4));
+            println!("*{}*", " ".repeat(len + 2));
+            println!("* {} *", output);
+            println!("*{}*", " ".repeat(len + 2));
+            println!("{}", "*".repeat(len + 4));
+        }
+    }
+    struct Point {
+        x: i32,
+        y: i32,
+    }
+
+    impl OutlinePrint for Point {}
+    impl fmt::Display for Point {
+        fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+            write!(f, "({}, {})", self.x, self.y)
+        }
+    }
+    println!("{}", Point{ x: 3, y: 4 })
+}
+/// newtype 模式用以在外部类型上实现外部 trait
+pub fn advanced_traits5 () {
+
+}
 /// 宏 <br>
 /// 使用 macro_rules! 的 声明宏 用于通用元编程<br>
 /// 声明宏允许我们编写一些类似 Rust match 表达式的代码。
