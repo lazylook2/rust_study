dcd81ff27c0108cad15d475338dbf701848b27f2
lazylook2 <15044013742@126.com>
2020-09-17 09:41:53 +0800

    高级类型，高级函数，闭包

--- src/advanced_features.rs
+++ src/advanced_features.rs
@@ -257,6 +257,8 @@ pub fn advanced_traits3(){
     println!("{}", <Dog as Animal>::baby_name("小狗"));
 }
 use std::fmt;
+use std::fmt::Formatter;
+
 /// 父 trait 用于在另一个 trait 中使用某 trait 的功能
 /// ???????????????????????????????????????
 pub fn advanced_traits4 (){
@@ -288,13 +290,216 @@ pub fn advanced_traits4 (){
     println!("{}", Point{ x: 3, y: 4 })
 }
 /// newtype 模式用以在外部类型上实现外部 trait
+/// 如果想要在 Vec<T> 上实现 Display，而孤儿规则阻止我们直接这么做，因为 Display trait 和 Vec<T> 都定义于我们的 crate 之外。
+/// 可以创建一个包含 Vec<T> 实例的 Wrapper 结构体
 pub fn advanced_traits5 () {
+    struct Wapper (Vec<String>);
+    impl fmt::Display for Wapper {
+        fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+            write!(f, "[{}]", self.0.join(", "))
+        }
+    }
+    // Display 的实现使用 self.0 来访问其内部的 Vec<T>，因为 Wrapper 是元组结构体而 Vec<T> 是结构体总位于索引 0 的项。
+    // 接着就可以使用 Wrapper 中 Display 的功能了。
 
+    let w = Wapper(vec![String::from("hello"), String::from("world")]);
+    println!("w = {}", w)
+}
+use std::error::Error;
+/// 类型别名用来创建类型同义词
+pub fn advanced_types1 () {
+    type Kilometers = i32;
+    let x: i32 = 5;
+    let y: Kilometers = 5;
+    println!("x + y = {}", x + y);
+
+// 用于代替很长的类型如：Box<dyn Fn() + Send + 'static>
+    /*type Thunk = Box<dyn Fn() + Send + 'static>;
+
+    let f: Thunk = Box::new(|| println!("hi"));
+
+    fn takes_long_type(f: Thunk) {
+        // --snip--
+    }
+
+    fn returns_long_type() -> Thunk {
+        // --snip--
+    }*/
+
+// 类型别名也经常与 Result<T, E> 结合使用来减少重复。
+
+    /*pub trait Write {
+        fn write(&mut self, buf: &[u8]) -> Result<usize, Error>;
+        fn flush(&mut self) -> Result<(), Error>;
+
+        fn write_all(&mut self, buf: &[u8]) -> Result<(), Error>;
+        fn write_fmt(&mut self, fmt: fmt::Arguments) -> Result<(), Error>;
+    }*/
+    type Result<T> = std::result::Result<T, std::io::Error>;
+    pub trait Write {
+        fn write(&mut self, buf: &[u8]) -> Result<usize>;
+        fn flush(&mut self) -> Result<()>;
+
+        fn write_all(&mut self, buf: &[u8]) -> Result<()>;
+        fn write_fmt(&mut self, fmt: fmt::Arguments) -> Result<()>;
+    }
+}
+/// 从不返回的 never type<br>
+/// Rust 有一个叫做 ! 的特殊类型。在类型理论术语中，它被称为 empty type，因为它没有值。<br>
+/// 我们更倾向于称之为 never type。<br>
+/// 这个名字描述了它的作用：在函数从不返回的时候充当返回值。
+pub fn advanced_types2() {
+    // 这读 “函数 bar 从不返回”，而从不返回的函数被称为 发散函数
+    // 不能创建 ! 类型的值，所以 bar 也不可能返回值。
+    //   fn bar() -> ! {}
+
+    let guess = "3";
+    loop {
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+        break;
+    };
+
+    /*let guess = match guess.trim().parse() {
+        Ok(_) => 5,
+        Err(_) => "hello",
+    };*/
+    // 上面的guess必须是整形也是字符串而rust要求guess只能是一个类型
+    // 那么continue返回了u32另一个分支却以continue结束呢
+    // continue的值是 ! 。也就是说rust计算guess的类型时，它查看这两个分支。
+    // 前者是 u32 值，而后者是 ! 值。因为 ! 并没有一个值，Rust 决定 guess 的类型是 u32。
+    // 描述 ! 的行为的正式方式是 never type 可以强转为任何其他类型。
+    //      允许 match 的分支以 continue 结束是因为 continue 并不真正返回一个值；
+    //      相反它把控制权交回上层循环，所以在 Err 的情况，事实上并未对 guess 赋值。
+
+    // never type 的另一个用途是 panic!。还记得 Option<T> 上的 unwrap 函数吗？它产生一个值或 panic。
+
+
+    /*impl<T> Option<T> {
+        pub fn unwrap(self) -> T {
+            match self {
+                Some(val) => val,
+                None => panic!("called `Option::unwrap()` on a `None` value"),
+            }
+        }
+    }*/
+    // 这里与示例 19-34 中的 match 发生了相同的情况：
+    //     Rust 知道 val 是 T 类型，panic! 是 ! 类型，所以整个 match 表达式的结果是 T 类型。
+    // 这能工作是因为 panic! 并不产生一个值；它会终止程序。
+    // 对于 None 的情况，unwrap 并不返回一个值，所以这些代码是有效。
+
+    // 最后一个有着 ! 类型的表达式是 loop：
+    print!("forever ");
+
+    // 这里，循环永远也不结束，所以此表达式的值是 !。但是如果引入 break 这就不为真了，因为循环在执行到 break 后就会终止。
+    loop {
+        print!("and ever ");
+    }
+}
+/// 高级函数与闭包
+/// 函数指针
+pub fn advanced_functions1() {
+    fn add_one(x: i32) -> i32 {
+        x + 1
+    }
+    fn do_twice(f: fn(i32) -> i32, arg: i32) -> i32 {
+        f(arg) + f(arg)
+    }
+    let answer = do_twice(add_one, 5);
+    println!("答案是： {}", answer);
+    // 这会打印出 The answer is: 12。do_twice 中的 f 被指定为一个接受一个 i32 参数并返回 i32 的 fn。
+    // 接着就可以在 do_twice 函数体中调用 f。
+    // 在 main 中，可以将函数名 add_one 作为第一个参数传递给 do_twice。
+
+    // 不同于闭包，fn 是一个类型而不是一个 trait，所以直接指定 fn 作为参数而不是声明一个带有 Fn 作为 trait bound 的泛型参数。
+    // 函数指针实现了所有三个闭包 trait（Fn、FnMut 和 FnOnce），所以总是可以在调用期望闭包的函数时传递函数指针作为参数。
+    // 倾向于编写使用泛型和闭包 trait 的函数，这样它就能接受函数或闭包作为参数。
+
+    fn wapper_func<T>(t: T, v: i32) -> i32
+        where T: Fn(i32) -> i32 {
+        t(v)
+    }
+
+    fn func(v: i32) -> i32 {
+        v + 1
+    }
+    //+++++++++++++++++
+    let a = wapper_func(|x| x+1, 1);
+    println!("a = {}", a);
+
+    let b = wapper_func(func, 1);
+    println!("b = {}", b);
+
+    // 使用 map 函数将一个数字 vector 转换为一个字符串 vector，就可以使用闭包，比如这样：
+    let list_of_numbers = vec![1, 2, 3];
+    let list_of_strings: Vec<String> = list_of_numbers
+        .iter().map(|i| i.to_string()).collect();
+    for s in list_of_strings.iter() { println!("{}\t", s); }
+
+    // 或者可以将函数作为 map 的参数来代替闭包，像是这样：
+    let list_of_numbers = vec![1, 2, 3];
+    let list_of_strings: Vec<String> = list_of_numbers
+        .iter()
+        .map(ToString::to_string)// ?????????????????
+        .collect();
+    for s in list_of_strings.iter() { println!("{}\t", s); }
+    // 注意这里必须使用 “高级 trait” 部分讲到的完全限定语法，因为存在多个叫做 to_string 的函数；
+    //     这里使用了定义于 ToString trait 的 to_string 函数，标准库为所有实现了 Display 的类型实现了这个 trait。
+
+    enum Status {
+        Value(u32),
+        Stop,
+    }
+
+    let list_of_statuses: Vec<Status> =
+        (0u32..20)
+            .map(Status::Value)
+            .collect();
+    // 这里创建了 Status::Value 实例，它通过 map 用范围的每一个 u32 值调用 Status::Value 的初始化函数。
+    // 一些人倾向于函数风格，一些人喜欢闭包。这两种形式最终都会产生同样的代码，所以请使用对你来说更明白的形式吧。
+
+    for s in list_of_statuses.iter() {
+        match s {
+            Status::Value(num) => println!("{}", num),
+            _ => {}
+        }
+    }
+
+
+
+}
+/// 返回闭包
+pub fn advanced_functions2() {
+    /*这段代码尝试直接返回闭包，它并不能编译：
+    fn returns_closure() -> Fn(i32) -> i32 {
+        |x| x + 1
+    }*/
+
+    // Rust 并不知道需要多少空间来储存闭包。不过我们在上一部分见过这种情况的解决办法：可以使用 trait 对象：
+    fn returns_closure() -> Box<dyn Fn(i32) -> i32> {
+        Box::new(|x| x + 1)
+    }
+    // 这段代码正好可以编译。
 }
 /// 宏 <br>
 /// 使用 macro_rules! 的 声明宏 用于通用元编程<br>
 /// 声明宏允许我们编写一些类似 Rust match 表达式的代码。
 pub fn macros1() {
-    
+    #[macro_export]
+    macro_rules! my_vec {
+        ( $( $x: expr ), * ) => {
+            {
+                let mut temp_vec = Vec::new();
+                $ (
+                    temp_vec.push($x);
+                )*
+                temp_vec
+            }
+        }
+    }
+    let v = my_vec![1, 2, 3];
+    println!("v = {:?}", v);
 
 }
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,9 @@
 #![allow(unused_variables)]
+
+use rust_study::advanced_features::{advanced_functions1, macros1};
+
 #[allow(unused_mut)]
-use rust_study::concurrency::threads1;
-use rust_study::concurrency::{threads2, threads3, move1, message_passing, message_passing2, message_passing3, mutex, mutex1};
-use rust_study::object_oriented::{oo1, oo2};
-use rust_study::patterns::{p1, p2};
-use rust_study::advanced_features::{unsafe_rust1, unsafe_rust3, unsafe_rust2, unsafe_rust4, unsafe_rust5, advanced_traits1, advanced_traits2, advanced_traits3, advanced_traits4};
 
 fn main() {
-    advanced_traits4()
+    macros1()
 }
